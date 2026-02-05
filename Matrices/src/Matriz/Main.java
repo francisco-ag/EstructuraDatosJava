77ba7ddc0bf35d4a8a491d521514dacd00631006
francisco-ag <francisamado80@gmail.com>
2026-02-05 13:05:42 -0600

    Matrices

--- Matrices/src/Matriz/Main.java
+++ Matrices/src/Matriz/Main.java
@@ -1,4 +1,31 @@
 package Matriz;
 
+import java.util.Scanner;
+
 public class Main {
+
+    // metodo Main
+    public static void main (String[] a){
+        Scanner sc = new Scanner(System.in);
+
+        // 2 filas x 3 columnas
+        String[][] matriz = new String[2][3];
+
+        for (int i=0; i<matriz.length; i++){
+            for(int j=0; j<matriz[i].length; j++){
+                System.out.println("Ingresa  el nombre en la posicion ["+i+"]["+j+"] : ");
+                matriz[i][j] = sc.next();
+            }
+        }
+
+        System.out.println("Matriz ingresada :");
+        for (int i=0; i<matriz.length; i++){
+            for(int j=0; j<matriz[i].length; j++){
+                System.out.print(matriz[i][j]+"\t");
+            }
+            System.out.println();
+        }
+
+    }
+
 }
