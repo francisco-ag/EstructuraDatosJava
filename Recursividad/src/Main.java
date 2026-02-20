525c6c730ccc38d94542eeea2ce597b2e2c55e9a
francisco-ag <francisamado80@gmail.com>
2026-02-20 08:59:55 -0600

    Serie de Fibonacci

--- Recursividad/src/Main.java
+++ Recursividad/src/Main.java
@@ -30,8 +30,41 @@ public class Main {
                    );
                    break;
                case 2:
+                   int numFib = Integer.parseInt(
+                           JOptionPane.showInputDialog(
+                                   "¿Cuantos numeros quieres de la serie?"
+                           )
+                   );
+
+                   StringBuilder sb = new StringBuilder(
+                           "Sucesión Fibonacci\n"
+                   );
+
+                   for (int i=0 ; i < numFib ; i++ ){
+                       sb.append( fibonacci(i) ).append("->");
+                   }
+
+                   JOptionPane.showMessageDialog(
+                           null,
+                           sb.toString()
+                   );
+
                    break;
                case 3:
+                   int n = Integer.parseInt(
+                           JOptionPane.showInputDialog("¿Cuantos elementos quieres guardar?")
+                   );
+                   int[] vector = new int[n];
+                   for ( int i=0; i<n ; i++){
+                       vector[i] = Integer.parseInt(
+                               JOptionPane.showInputDialog( "Elemento "+(i+1)+" : ")
+                       );
+                   }
+                   int suma = sumaVectorRecursiva(vector,0);
+                   JOptionPane.showMessageDialog(
+                           null,
+                           "La suma del vector es "+suma
+                   );
                    break;
                case 4:
                    String texto = JOptionPane.showInputDialog(
@@ -60,4 +93,22 @@ public class Main {
         return invertir( texto.substring(1) )
                 + texto.charAt(0);
     }
+
+    public static int sumaVectorRecursiva (
+            int[] vector,
+            int indice
+    ){
+        if( indice == vector.length ) return  0;
+        return  vector[indice] + sumaVectorRecursiva(vector,indice+1);
+    }
+
+    public static int fibonacci ( int numero){
+        if ( numero == 0 ) return  0;
+        if ( numero == 1 ) return  1;
+        return fibonacci( numero - 1)
+                + fibonacci( numero - 2) ;
+    }
+
+
+
 }
