c2d3af845e86fa63a4307fdefaae103d80e77961
francisco-ag <francisamado80@gmail.com>
2026-04-15 08:57:34 -0600

    Main Arboles

--- Arboles/src/arbol/Main.java
+++ Arboles/src/arbol/Main.java
@@ -1,17 +1,52 @@
 package arbol;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Arbol myArbol = new Arbol();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int opcion=0;
+
+        do{
+            String menu = " === MENU ARBOLES ===\n"
+                    +"( 1 ) Insertar \n"
+                    +"( 2 ) Buscar \n"
+                    +"( 3 ) Recorrido inOrden \n"
+                    +"( 9 ) Salir \n"
+                    +"Selecciona una opción";
+            opcion = Integer.parseInt(JOptionPane.showInputDialog(menu));
+            switch (opcion){
+                case 1:
+                    int valor = Integer.parseInt(
+                            JOptionPane.showInputDialog(
+                                    "Ingresa un numero"
+                            )
+                    );
+                    myArbol.insertar(valor);
+                    break;
+                case 2:
+                    int valorBuscar = Integer.parseInt(
+                            JOptionPane.showInputDialog(
+                                    "Ingresa un numero a Buscar"
+                            )
+                    );
+                    boolean encontrado = myArbol.buscar(valorBuscar);
+                    JOptionPane.showMessageDialog(
+                            null,
+                            encontrado ? "Numero encontrado" : "No encontrado"
+                    );
+                    break;
+                case 3:
+                    JOptionPane.showMessageDialog(
+                            null,
+                            "Recorrido inOrden"+myArbol.inOrden()
+                    );
+
+            }
+
+
+        }while(opcion != 9);
+
     }
 }
